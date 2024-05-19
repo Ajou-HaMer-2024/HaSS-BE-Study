7d16ee49a3eb726076e6d3d365132487b115e67a
JungJoonWoo <jjw2030@ajou.ac.kr>
2024-05-19 23:54:09 +0900

    2주차 스터디

---
+++ 1주차/정준우/hello/src/main/java/hello/core/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.repository.MemberRepository;
+import hello.core.repository.MemoryMemberRepository;
+import hello.core.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+  @Bean
+  public MemberService memberService() {
+    return new MemberService(memberRepository());
+  }
+
+  @Bean
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+}
---
+++ 1주차/정준우/hello/src/main/java/hello/core/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package hello.core.repository;
+
+import hello.core.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+  Member save(Member member);
+
+  Optional<Member> findById(Long id);
+
+  Optional<Member> findByName(String name);
+
+  List<Member> findAll();
+}
